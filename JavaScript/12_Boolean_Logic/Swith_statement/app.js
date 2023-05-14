4208b198537527aacc72a4fb4ff2f6e1a91d4504
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-05-14 10:34:59 -0400

    break&merge cases

--- JavaScript/12_Boolean_Logic/Swith_statement/app.js
+++ JavaScript/12_Boolean_Logic/Swith_statement/app.js
@@ -1,14 +1,23 @@
-const day = 2;
+const day = 6;
 switch (day) {
     case 1:
         console.log("Monday");
+        break;
     case 2:
         console.log("Tuesday");
+        break;
     case 3:
         console.log("Wendensday");
+        break;
     case 4:
         console.log("Thursday");
+        break;
     case 5:
         console.log("Friday");
+        break;
+    case 6:
+    case 7:
+        console.log("Weekend!!!");
+        break;
 
 }
