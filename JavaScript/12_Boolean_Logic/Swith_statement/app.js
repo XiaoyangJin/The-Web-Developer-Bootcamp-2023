780a333b7c7d50d98bdc43c00ee51c017c0972ab
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-05-14 10:32:04 -0400

    no break switch

---
+++ JavaScript/12_Boolean_Logic/Swith_statement/app.js
@@ -0,0 +1,14 @@
+const day = 2;
+switch (day) {
+    case 1:
+        console.log("Monday");
+    case 2:
+        console.log("Tuesday");
+    case 3:
+        console.log("Wendensday");
+    case 4:
+        console.log("Thursday");
+    case 5:
+        console.log("Friday");
+
+}
