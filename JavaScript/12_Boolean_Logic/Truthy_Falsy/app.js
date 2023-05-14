c36899936a41dba7cb266ca41554dcd946742142
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-05-14 10:25:29 -0400

    falsy

--- JavaScript/12_Boolean_Logic/Truthy_Falsy/app.js
+++ JavaScript/12_Boolean_Logic/Truthy_Falsy/app.js
@@ -1,32 +1,38 @@
 const userInput = prompt("Enter something");
 
 //this is count as falsy if nothing be input
-if (userInput) {
-    console.log("TRUTHY!")
+// if (userInput) {
+//     console.log("TRUTHY!")
+// } else {
+//     console.log("FALSY!")
+// }
+
+if (0) {
+    console.log("TRUTHY")
 } else {
-    console.log("FALSY!")
+    console.log("FALSY")
 }
 
-// if (0) {
-//     console.log("TRUTHY")
-// } else {
-//     console.log("FALSY")
-// }
+if (NaN) {
+    console.log("TRUTHY")
+} else {
+    console.log("FALSY")
+}
 
-// if (NaN) {
-//     console.log("TRUTHY")
-// } else {
-//     console.log("FALSY")
-// }
+if (undefined) {
+    console.log("TRUTHY")
+} else {
+    console.log("FALSY")
+}
 
-// if (undefined) {
-//     console.log("TRUTHY")
-// } else {
-//     console.log("FALSY")
-// }
+if ('') {
+    console.log("TRUTHY")
+} else {
+    console.log("FALSY")
+}
 
-// if (' ') {
-//     console.log("TRUTHY")
-// } else {
-//     console.log("FALSY")
-// }
+if (' ') {
+    console.log("TRUTHY")
+} else {
+    console.log("FALSY")
+}
